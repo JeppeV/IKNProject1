916ccb3f0989a8edc71b5379946b9a5801f37320
JeppeV <Jeppevinberg@gmail.com>
2016-02-24 12:00:52 +0100

    stuffs all over

--- file_client.py
+++ file_client.py
@@ -1,15 +1,21 @@
 import sys
-from socket import *
+import socket
 from lib import Lib
 
 PORT = 9000
 BUFSIZE = 1000
 
 def main(argv):
+    servername = argv[0];
+    filename = argv[1];
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((servername, PORT))
+    client_socket.send(filename)
+    data = client_socket.recv(BUFSIZE)
 
-    
+    client_socket.close()
 def receiveFile(fileName,  conn):
-	# TO DO Your Code
+   print
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
--- file_server.py
+++ file_server.py
@@ -6,17 +6,40 @@ HOST = ''
 PORT = 9000
 BUFSIZE = 1000
 
-def main(argv):
-	serversocket = socket.socket(
-      socket.AF_INET, socket.SOCK_STREAM)
-   #bind the socket to a public host,
-   # and a well-known port
-   serversocket.bind((socket.gethostname(), 80))
-   #become a server socket
-   serversocket.listen(5)
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
 
-def sendFile(fileName,  fileSize,  conn):
-	# TO DO Your Code
+    while 1:
+        connection_socket, address = server_socket.accept()
+        file_name = connection_socket.recv(BUFSIZE)
+
+        file_name = file_name.lower()
+        file_size = Lib.check_File_Exists(file_name)
+        if file_size != 0:
+            send_file(file_name, 1, connection_socket)
+
+
+    connection_socket.close()
+
+def send_file(file_name,  file_size,  conn):
+    file = open(file_name, "rb")
+    for piece in read_in_chunks(file):
+        conn.sendall(piece)
+
+
+def read_in_chunks(file_object):
+    chunk_size = 1024
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+
+
+
     
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main()
