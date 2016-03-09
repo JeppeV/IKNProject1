91f95f0e424d9d6d5b93c7db09c0e5db11489d39
Frederik <frederik290@gmail.com>
2016-03-09 15:12:12 +0100

    the udp again

--- UDP/UDP_client.py
+++ UDP/UDP_client.py
@@ -1,16 +1,17 @@
 import socket
+import sys
 PORT = 9000
 
 def main(argv):
     servername = argv[0]
+    command = argv[1]
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    command = raw_input("Enter command (u or l) ")
     client_socket.sendto(command, (servername, PORT))
     output, server_address = client_socket.recvfrom(1000)
-    print "You specified the command ", command, " and received: " , output
+    print "You specified the command ", command, " and received: ", output
     client_socket.close()
 
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
--- UDP/UDP_server.py
+++ UDP/UDP_server.py
@@ -5,21 +5,18 @@ PORT = 9000
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(HOST, PORT)
+    server_socket.bind((HOST, PORT))
     print "The server is ready to receive"
 
     while 1:
-        message, clientAddress = server_socket.recvfrom(1000)
+        message, client_address = server_socket.recvfrom(1000)
         message = message.lower()
         if message == "u":
-            file = open("/proc/uptime", "rb")
-            data = file.readline()
-            server_socket.sendto(data, clientAddress)
-        if message == "l":
-            file = open("/proc/loadavg", "rb")
-            data = file.readline()
-            server_socket.sendto(data,clientAddress)
-
+            file_object = open("/proc/uptime", "rb")
+        elif message == "l":
+            file_object = open("/proc/loadavg", "rb")
+        data = file_object.readline()
+        server_socket.sendto(data, client_address)
 
 if __name__ == "__main__":
     main()
