f5c492a0f7a2b68c682f1adbfaf9fa17dd8ca31f
Frederik <frederik290@gmail.com>
2016-03-10 13:17:58 +0100

    UDP

--- UDP/UDP_server.py
+++ UDP/UDP_server.py
@@ -13,13 +13,16 @@ def main():
         message = message.lower()
         if message == "u":
             file_object = open("/proc/uptime", "rb")
-	    data = file_object.readline()
+            data = file_object.readline()
         elif message == "l":
             file_object = open("/proc/loadavg", "rb")
-	    data = file_object.readline()
+            data = file_object.readline()
         else:
-	    data = "ERR" 	
+            data = "ERR"
         server_socket.sendto(data, client_address)
+        if file_object:
+            file_object.close()
+
 
 if __name__ == "__main__":
     main()
